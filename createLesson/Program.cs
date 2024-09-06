7b7775f4c88cb246096468d93d1fe37494f4cf8b
Hardeep Singh <HardeepDive@gmail.com>
2024-09-06 16:15:16 +0700

    20240906

--- createLesson/Program.cs
+++ createLesson/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Globalization;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using System.Text.Json;
 using Excel = Microsoft.Office.Interop.Excel;       //microsoft Excel 14 object in references-> COM tab
 
@@ -29,10 +28,10 @@ namespace Sandbox
 
         public class Sel
         {
-     
-            public int choice { get; set; }
-            
-            public string description { get; set; } 
+
+            public string choice { get; set; }
+
+            public string description { get; set; }
         }
 
 
@@ -45,9 +44,9 @@ namespace Sandbox
 
             public string question { get; set; }
 
-            public List<Sel> selections  { get; set; }
+            public List<Sel> selections { get; set; }
 
-            public int answer { get; set; }
+            public string answer { get; set; }
 
             public string reason { get; set; }
 
@@ -75,23 +74,23 @@ namespace Sandbox
 
 
 
-            JsonConversation();
+             JsonConversation();
 
            // JsonQuiz();
         }
-        
+
         public static void JsonConversation()
         {
 
             //Create COM Objects. Create a COM object for everything that is referenced
             Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\data\inglesGuru\lesson1ver12.xlsx");
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\data\inglesGuru\L0103B.xlsx");
             Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
             Excel.Range xlRange = xlWorksheet.UsedRange;
 
             int rowCount = xlRange.Rows.Count;
 
-            string colValue = "";
+           // string colValue = "";
 
             Pages pg = new Pages();
             pg.pages = new List<Page>();
@@ -147,15 +146,15 @@ namespace Sandbox
                 }
 
 
-                    }
+            }
 
 
 
 
-       
+
             var json = JsonSerializer.Serialize(pg);
 
-            File.WriteAllText(@"C:\data\inglesGuru\lesson1.json", json);
+            File.WriteAllText(@"C:\data\inglesGuru\L0103B.json", json);
 
 
 
@@ -185,113 +184,122 @@ namespace Sandbox
 
             //Create COM Objects. Create a COM object for everything that is referenced
             Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\data\inglesGuru\lesson1ver12.xlsx");
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\data\inglesGuru\content.xlsx");
             Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[2];
             Excel.Range xlRange = xlWorksheet.UsedRange;
 
             int rowCount = xlRange.Rows.Count;
 
-    
+
 
             Quizes qz = new Quizes();
             qz.quizes = new List<Quiz>();
 
             int cntQuizes = 0;
-            
+
 
             //iterate over the rows and columns and print to the console as it appears in the file
             //excel is not zero based!!
             for (int i = 1; i <= rowCount; i++)
             {
                 //write the value to the console
-                if (xlRange.Cells[i, 1] != null && xlRange.Cells[i, 1].Value2 != null)
+                //if (xlRange.Cells[i, 1] != null && xlRange.Cells[i, 1].Value2 != null)
+                //{
+
+                string context = "";
+                string question = "";
+                string answer = "";
+                string option = "";
+                string reason = "";
+
+                if (xlRange.Cells[i, 1].Value2 != null)
                 {
+                    context = xlRange.Cells[i, 1].Value2.ToString();
+                }
 
-                    string context = "";
-                    string question = "";
-                    string answer = "";
-                    string option = "";
-                    string reason = "";
+                if (context == "Context")
+                {
+              
+                //    if (qz.quizes.Count != 0)
+                //    {
+                //        qz.quizes[cntQuizes - 1].context = qz.quizes[cntQuizes - 1].context + "</ol>";
+                //    }
+                            
+                    qz.quizes.Add(new Quiz());
 
-                    if (xlRange.Cells[i, 1].Value2 != null)
-                    {
-                        context = xlRange.Cells[i, 1].Value2.ToString();
-                    }
+                    cntQuizes++;
+                    qz.quizes[cntQuizes - 1].nbr = cntQuizes;
+                    qz.quizes[cntQuizes - 1].context = "";
+                    qz.quizes[cntQuizes - 1].question = "";
+                    qz.quizes[cntQuizes - 1].selections = new List<Sel>();
+                    qz.quizes[cntQuizes - 1].answer = "";
+                    qz.quizes[cntQuizes - 1].reason = "";
 
-
-                    if (xlRange.Cells[i, 2].Value2 != null)
-                    {
-                        question = xlRange.Cells[i, 2].Value2.ToString();
-                    }
-
-                    if (xlRange.Cells[i, 3].Value2 != null)
-                    {
-                        option = xlRange.Cells[i, 3].Value2.ToString();
-                    }
-
-                    if (xlRange.Cells[i, 4].Value2 != null)
-                    {
-                        answer = xlRange.Cells[i, 4].Value2.ToString();
-                    }
-
-                    if (xlRange.Cells[i, 5].Value2 != null)
-                    {
-                        reason = xlRange.Cells[i, 5].Value2.ToString();
-                    }
-
-                    if (question == "Question")
-                    {
-                        qz.quizes.Add(new Quiz());
-                        
-                        cntQuizes++;
-                        qz.quizes[cntQuizes - 1].nbr = cntQuizes;
-                        qz.quizes[cntQuizes - 1].context = "";
-                        qz.quizes[cntQuizes - 1].question = "";
-                        qz.quizes[cntQuizes - 1].selections = new List<Sel>();
-                        qz.quizes[cntQuizes - 1].answer = 0;
-                        qz.quizes[cntQuizes - 1].reason = "";
-
-                    }
-                    else
-                    {
-                        if (context != "")
-                        {
-                            qz.quizes[cntQuizes - 1].context = qz.quizes[cntQuizes - 1].context + context + "\n";
-                        }
-
-                        if (question != "")
-                        {
-                            qz.quizes[cntQuizes - 1].question = question;
-                        }
-
-                        if (option != "")
-                       {
-                            //option = new Option(1,"");
-
-                            var sel = new Sel();
-                            sel.choice = qz.quizes[cntQuizes - 1].selections.Count + 1;
-                            sel.description = option;
-                            qz.quizes[cntQuizes - 1].selections.Add(sel);
-
-                        }
-
-                        if (answer != "")
-                        {
-                            qz.quizes[cntQuizes - 1].answer = int.Parse(answer);
-
-                        }
-
-                        if (reason != "")
-                        {
-                            qz.quizes[cntQuizes - 1].reason = qz.quizes[cntQuizes - 1].reason + reason + "\n";
-                        }
+                    continue;
+                }
 
 
-                    }
 
 
+                if (xlRange.Cells[i, 2].Value2 != null)
+                {
+                    question = xlRange.Cells[i, 2].Value2.ToString();
+                }
+
+                if (xlRange.Cells[i, 3].Value2 != null)
+                {
+                    option = xlRange.Cells[i, 3].Value2.ToString();
+                }
+
+                if (xlRange.Cells[i, 4].Value2 != null)
+                {
+                    answer = xlRange.Cells[i, 4].Value2.ToString();
+                }
+
+                if (xlRange.Cells[i, 5].Value2 != null)
+                {
+                    reason = xlRange.Cells[i, 5].Value2.ToString();
+                }
+
+
+                if (context != "")
+                {
+                    qz.quizes[cntQuizes - 1].context =  qz.quizes[cntQuizes - 1].context + context ;
+                }
+
+                if (question != "")
+                {
+                    qz.quizes[cntQuizes - 1].question =  question;
+                }
+
+                if (option != "")
+                {
+                    //option = new Option(1,"");
+
+                    var sel = new Sel();
+                    sel.choice = (qz.quizes[cntQuizes - 1].selections.Count + 1).ToString();
+                    sel.description = option;
+                    qz.quizes[cntQuizes - 1].selections.Add(sel);
 
                 }
+
+                if (answer != "")
+                {
+                    qz.quizes[cntQuizes - 1].answer = answer;
+
+                }
+
+                if (reason != "")
+                {
+                    qz.quizes[cntQuizes - 1].reason = qz.quizes[cntQuizes - 1].reason + reason;
+                }
+
+
+
+
+
+
+                //  }
 
 
             }
--- createLesson/obj/Debug/net6.0/createLesson.AssemblyInfo.cs
+++ createLesson/obj/Debug/net6.0/createLesson.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("createLesson")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4a2b9680aa0a1f7112011330114ef1fe6b66a18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af8611bc1374789f517648f17f512d1875b77f82")]
 [assembly: System.Reflection.AssemblyProductAttribute("createLesson")]
 [assembly: System.Reflection.AssemblyTitleAttribute("createLesson")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
